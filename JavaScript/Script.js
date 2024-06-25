dc4e445a39317a9c94b5916ce3f305b0f65a28e7
Yisthepath <130940873+Yisthepath@users.noreply.github.com>
2024-06-25 12:44:35 +0100

    Modified the code to make the computer choice more
    random and to announce the result of the game after
    
    getting to a score of 5

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,11 +1,15 @@
+//Declare the variables used to store the computer and the player's choices as well as to keep score// 
+
 let getHumanChoice;
 let humanScore = 0;
 let computerScore = 0;
 let computerChoice;
 let roundOutcome;
 
+//Write a function to get a random choice by the computer//
+
 function getComputerChoice() { 
-    var x = Math.floor(Math.random() * 2.5);
+    var x = Math.floor(Math.random() * 3);
 
     switch (x) {
         case 0:
@@ -22,6 +26,8 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
+
+//Write a funtion that decides of each round outcome and writes a paragrapg on the DOM accordingly//
 
 function gameOutcome() {
     if (getHumanChoice.toUpperCase() === computerChoice.toUpperCase()) {
@@ -68,18 +74,34 @@ function gameOutcome() {
 
     const p = document.querySelector("p");
     
-    if (roundOutcome === "WIN") {
-        p.textContent = "Congratulations, you won! Your score: " + humanScore + " Computer score : " + computerScore; 
+    if (humanScore === 5) {
+        p.textContent = "Congratulations, you won! choose an option to play again.";
+        humanScore = 0;
+        computerScore = 0;
     }
 
-    else if (roundOutcome === "LOSE") {
-        p.textContent = "The computer won. Your score: " + humanScore + " Computer score : " + computerScore;
+    else if (computerScore === 5) {
+        p.textContent = "The computer won. choose an option to try again.";
+        humanScore = 0;
+        computerScore = 0;
     }
 
     else {
-        p.textContent = "It's a tie. Your score: " + humanScore + " Computer score : " + computerScore;
+        if (roundOutcome === "WIN") {
+            p.textContent = "You won the round! Your score: " + humanScore + " Computer score : " + computerScore; 
+        }
+
+        else if (roundOutcome === "LOSE") {
+            p.textContent = "The computer won the round. Your score: " + humanScore + " Computer score : " + computerScore;
+        }
+
+        else {
+            p.textContent = "It's a tie. Your score: " + humanScore + " Computer score : " + computerScore;
+        }
     }
 };
+
+//Add event listeners to attribute a value to the variable storing the player's choice (getHumanChoice)//
 
 const rock = document.querySelector("#rock");
 rock.addEventListener("click", () => {
